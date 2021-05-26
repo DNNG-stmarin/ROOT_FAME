9d75454de86eeeb5858a95f7b9221e873407700e
um-giha <giha@umich.edu>
2021-05-26 13:34:39 -0500

    Completed InitializeFunctions.C and implemented in BeamAnalysis.C
    Made function array for fitting each PPAC independently and implemented inside BeamAnalysis.C

--- readFiss/src/BeamAnalysis.C
+++ readFiss/src/BeamAnalysis.C
@@ -43,10 +43,20 @@ void readFiss::BeamDepAnalysis()
     cout << "subtracted histograms" << endl;
 
   // fit the alpha background This could be its own function
-    double MaxBinCount = h_alphaDep[r]->GetMaximumBin();									//Use GetMaximumBin to find candidate for peak (most events/counts)
+    double maxCountBin = h_alphaDep[r]->GetMaximumBin();									//Use GetMaximumBin to find candidate for peak (most events/counts)
 
    // JAMES & NATHAN: make an array of functions and FitResultsPtr in readFiss.h, declare them in a new file initializeFunctions.h, and save the fits for each
-   //  f_alphaBackground->SetRange(h_alphaSpec->GetBinCenter(MaxBinCount),BIN_ERG_MAX);	//Set alpha fit range to start at middle of peak of data
+   f_alpha[r]->SetRange(h_alphaDep[r]->GetBinCenter(maxCountBin), DEP_MAX);
+   h_alphaDep[r]->Fit((TString)"f_alpha" + (TString)to_string(r));
+   f_expo[r]->SetParameters(f_alpha[r]->GetParameter(0), f_alpha[r]->GetParameter(1));
+
+   h_fisSubtract[r]->Fit((TString)"f_fisProducts" + (TString)to_string(r));
+   f_gauss[r]->SetParameters(f_fisProducts[r]->GetParameter(0),
+                             f_fisProducts[r]->GetParameter(1),
+                             f_fisProducts[r]->GetParameter(2));
+
+
+   //  f_alphaBackground->SetRange(h_alphaSpec->GetBinCenter(maxCountBin),BIN_ERG_MAX);	//Set alpha fit range to start at middle of peak of data
    //  fitAlphas = h_alphaSpec->Fit("f_alphaBackground","RS");				//Initialize fit of alphaBackground in specified range (after peak) to ptr allowing for extraction of the variables of the fit
    //  expoConst = fitAlphas->Value(0);												//Get constant of exponential fit
    //  expoSlope = fitAlphas->Value(1);												//Get slope of exponential fit
