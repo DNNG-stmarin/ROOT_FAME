e1e38144967f67516c8c968cfd91f028e28da46f
Katie Ballard <bllrdk@umich.edu>
2021-07-01 11:04:56 -0400

    Added the parameter file output into a new file

--- src/DetectionImport.C
+++ src/DetectionImport.C
@@ -2,10 +2,6 @@
 Author: Stefano Marin, Katie Ballard 
 Purpose: Perform the analyisis of the collected data on a detector-by-detector basis.
 The result of this code will be a series of cuts to clean the data.
-
-So far, we look at:
-- PSD characterization
-- Timing delays
 */
 
 #define DetectionAnalysis_cxx
@@ -139,41 +135,6 @@ for(int det = 0; det < NUM_DETS; ++det){
     detectors[det].sigPhot = (TF1*)f_sigPhot->Clone(); 
 
 }
-
-return 1; 
-
-}
-
-// stores the parameters for the parameter fits into seperate text documents
-int DetectorSystemClass::ParamFiles()
-{
-
-cout << "Writing parameter files" << endl;
-
-// creates the output files and opens them 
-ofstream meanNeutFile;
-meanNeutFile.open("meanNeutParams");
-ofstream meanPhotFile; 
-meanPhotFile.open("meanPhotParams");
-ofstream sigNeutFile; 
-sigNeutFile.open("sigNeutParams");
-ofstream sigPhotFile;
-sigPhotFile.open("sigPhotParams");
-
-// loops through and saves the data for each detector
-for(int det = 0; det < NUM_DETS; ++det)
-{
-    meanNeutFile << det << " " << detectors[det].meanNeut->GetParameter(0) << " " << detectors[det].meanNeut->GetParameter(1)  << " " << detectors[det].meanNeut->GetParameter(2)  << " " << detectors[det].meanNeut->GetParameter(3)  << endl;
-    meanPhotFile << det << " " << detectors[det].meanPhot->GetParameter(0) << " " << detectors[det].meanPhot->GetParameter(1)  << " " << detectors[det].meanPhot->GetParameter(2)  << " " << detectors[det].meanPhot->GetParameter(3)  << endl;
-    sigNeutFile << det << " " << detectors[det].sigNeut->GetParameter(0) << " " << detectors[det].sigNeut->GetParameter(1)  << " " << detectors[det].sigNeut->GetParameter(2)  << " " << detectors[det].sigNeut->GetParameter(3)  << endl;
-    sigPhotFile << det << " " << detectors[det].sigPhot->GetParameter(0) << " " << detectors[det].sigPhot->GetParameter(1)  << " " << detectors[det].sigPhot->GetParameter(2)  << " " << detectors[det].sigPhot->GetParameter(3)  << endl;
-}
-
-// closes the files 
-meanNeutFile.close();
-meanPhotFile.close();
-sigNeutFile.close();
-sigPhotFile.close();
 
 return 1; 
 
